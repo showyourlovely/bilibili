ba88a475e4c5128f64caa861bff05a0eb5300edd
houdidi <193096567@qq.com>
2018-03-14 17:22:47 +0800

    合并后第一次bug

--- wanrui/src/App.vue
+++ wanrui/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <topHeader id="top-header"/>
-=======
->>>>>>> 045126ac0841b1285ed0f9c8130fe6840aab2c68
     <router-view/>
   </div>
 </template>
@@ -23,7 +20,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-<<<<<<< HEAD
   text-align: center;
   color: #2c3e50;
 }
@@ -31,9 +27,6 @@ export default {
   html{font-size: 26.67vw;}
   body{font-size: 0.16rem;}
   #top-header{width:100%;position:fixed;top: 0;left: 0;background: #fff;z-index: 9999;}
-=======
-}
   html,body{height: 100%;margin: 0;background: #f5f5f5;}
   html{font-size: 26.67vw}
->>>>>>> 045126ac0841b1285ed0f9c8130fe6840aab2c68
 </style>
--- wanrui/src/router/index.js
+++ wanrui/src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
 import index from '@/components/index'
 import download from '@/components/download'
+import HelloWorld from '@/components/HelloWorld'
+import Regist from '@/components/Regist'
+import Login from '@/components/Login'
+import Loginbf from '@/components/Loginbf'
 Vue.use(Router)
 var router = new Router({
   mode: "history",
@@ -16,21 +19,6 @@ var router = new Router({
       path: '/download',
       name: 'download',
       component: ()=>import('../components/download')
-=======
-import HelloWorld from '@/components/HelloWorld'
-import Regist from '@/components/Regist'
-import Login from '@/components/Login'
-import Loginbf from '@/components/Loginbf'
-
-Vue.use(Router)
-
-export default new Router({
-	mode: "history",
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
     },
     {
       path: '/regist',
@@ -46,7 +34,6 @@ export default new Router({
       path: '/loginbf',
       name: 'Loginbf',
       component: Loginbf
->>>>>>> 045126ac0841b1285ed0f9c8130fe6840aab2c68
     }
   ]
 })
